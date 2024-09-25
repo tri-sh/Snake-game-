91a4cbab60657d01d54800da4ff61e7569cf5b77
Trishita Das <das2trishita3@gmail.com>
2024-09-25 21:55:36 +0530

    modifies to store previous highscores

--- scoreboard.py
+++ scoreboard.py
@@ -7,8 +7,9 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
-        self.color("white")
+        with open("data.txt") as data:
+           self.high_score = int (data.read())
+        self.color("white") 
         self.penup()
         self.goto(8, 270)
         self.hideturtle()
@@ -21,6 +22,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
         
